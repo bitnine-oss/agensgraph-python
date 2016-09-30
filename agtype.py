6a3e8c495707973920e52bb973d7fa06c9282a1a
Kyungtae Lee <ktlee@bitnine.net>
2016-09-30 22:27:40 +0900

    initial commit for agens-graph's type

---
+++ agtype.py
@@ -0,0 +1,115 @@
+import re, json
+
+_v_matcher = re.compile(r'(.+)\[(\d+)\.(\d+)\](.+)')
+_e_matcher = re.compile(r'(.+)\[(\d+)\.(\d+)\]\[(\d+)\.(\d+),(\d+)\.(\d+)\](.*)')
+
+class GID:
+    def __init__(self, oident, ident):
+        self.oid = oident
+        self.id = ident
+    def __repr__(self):
+        return "[" + self.oid + "." + self.id + "]"
+
+class Vertex:
+    def __init__(self, value):
+        m = _v_matcher.match(value)
+        if m == None:
+            return ValueError
+        self.label = m.group(1)
+        self.vid = GID(m.group(2), m.group(3))
+        self.props = json.loads(m.group(4))
+    def __repr__(self):
+        return self.label + str(self.vid) + json.dumps(self.props)
+
+def _cast_vertex(value, cur):
+    if value is None:
+        return None
+    try:
+        v = Vertex(value)
+    except:
+        return psycopg2.InterfaceError("bad vertex representation: %s" % value)
+    return v
+
+class Edge:
+    def __init__(self, value):
+        m = _e_matcher.match(value)
+        if m == None:
+            raise ValueError
+        self.label = m.group(1)
+        self.eid = GID(m.group(2), m.group(3))
+        self.svid = GID(m.group(4), m.group(5))
+        self.evid = GID(m.group(6), m.group(7))
+        self.props = json.loads(m.group(8))
+    def __repr__(self):
+        svid = str(self.svid)[0:-1]
+        evid = str(self.evid)[1:]
+        return self.label + str(self.eid) + svid + ',' + evid + json.dumps(self.props)
+
+def _cast_edge(value, cur):
+    if value is None:
+        return None
+    try:
+        e = Edge(value)
+    except:
+        return psycopg2.InterfaceError("bad edge representation: %s" % value)
+    return e
+
+class Path:
+    def __init__(self, value):
+        l = self._tokenize(value)
+        if l == None:
+            raise ValueError
+        self.vertices = map(Vertex, l[0::2])
+        self.edges = map(Edge, l[1::2])
+    def _tokenize(self, value):
+        i = 0
+        s = 0
+        depth = 0
+        inGID = False
+        l = []
+        v = value[1:-1] # remove '[' and ']'
+        for c in v:
+            if '{' == c:
+                depth += 1
+            elif '}' == c:
+                depth -= 1
+            elif 0 == depth and '[' == c: # for GID
+                inGID = True
+            elif inGID and ']' == c:
+                inGID = False
+            elif 0 == depth and False == inGID and ',' == c:
+                l.append(v[s:i])
+                s = i + 1
+            if depth < 0:
+                raise ValueError
+            i += 1
+        l.append(v[s:i])
+        return l
+    def __repr__(self):
+        return ''.join(map(lambda x,y:repr(x)+repr(y),self.vertices,self.edges))[0:-4]
+    def start(self):
+        return self.vertices[0]
+    def end(self):
+        return self.vertices[-1]
+    def len(self):
+        return len(self.edges)
+
+def _cast_path(value, cur):
+    if value is None:
+        return None
+    try:
+        p = Path(value)
+    except:
+        return psycopg2.InterfaceError("bad path representation: %s" % value)
+    return p
+
+import psycopg2
+
+VERTEX = psycopg2.extensions.new_type((7012,), "VERTEX", _cast_vertex)
+psycopg2.extensions.register_type(VERTEX)
+
+EDGE = psycopg2.extensions.new_type((7022,), "EDGE", _cast_edge)
+psycopg2.extensions.register_type(EDGE)
+
+PATH = psycopg2.extensions.new_type((7032,), "PATH", _cast_path)
+psycopg2.extensions.register_type(PATH)
---
+++ test_agtype.py
@@ -0,0 +1,185 @@
+import unittest
+import psycopg2
+from psycopg2.extras import Json
+import agtype
+
+class BasicTest(unittest.TestCase):
+    def setUp(self):
+        self.conn = psycopg2.connect("dbname=test user=postgres")
+        self.cur = self.conn.cursor()
+        try:
+            self.cur.execute("drop graph p cascade")
+        except:
+            self.conn.commit()
+        self.cur.execute("create graph p")
+        self.cur.execute("set graph_path = p")
+        self.conn.commit()
+    def tearDown(self):
+        self.conn.commit()
+        self.cur.execute("drop graph p cascade")
+        self.cur.close()
+        self.conn.close()
+
+class CreateTest(BasicTest):
+    def setUp(self):
+        BasicTest.setUp(self)
+        self.cur.execute("create vlabel person")
+        self.cur.execute("create vlabel company")
+        self.cur.execute("create elabel employee")
+    def test_CreateWithBind(self):
+        self.cur.execute(
+                "CREATE ( :person { 'name': 'XXX', 'from': 'Sweden', 'klout': %s } )", (99,))
+        self.cur.execute("MATCH (n:person {'name': %s}) return n", ('XXX',))
+        n = self.cur.fetchone()[0]
+        self.assertEquals(99, n.props["klout"])
+    def test_CreateWithBindPrimitiveProp(self):
+        with self.assertRaises(psycopg2.ProgrammingError):
+            self.cur.execute("CREATE ( :person %s )", (Json(10),));
+    def test_CreateWithBindWholeProp(self):
+        self.cur.execute("CREATE ( :person %s )", 
+                (Json({'name': 'ktlee', 'from': 'Korea', 'klout': 17}),))
+        self.cur.execute("MATCH (n:person {'name': %s}) return n", ('ktlee',))
+        n = self.cur.fetchone()[0]
+        self.assertEquals(17, n.props["klout"])
+
+class LabelInheritTest(BasicTest):
+    def setUp(self):
+        BasicTest.setUp(self)
+        self.cur.execute("create vlabel parent")
+        self.cur.execute("create vlabel child inherits (parent)")
+        self.cur.execute("create (:parent {'name': 'father'})")
+        self.cur.execute("create (:child {'name': 'son'})")
+        pass
+    def test_MultiLable(self):
+        self.cur.execute("MATCH (x:parent) RETURN x ORDER BY (x).name")
+        x = self.cur.fetchone()[0]
+        self.assertEquals("father", x.props["name"])
+        x = self.cur.fetchone()[0]
+        self.assertEquals("son", x.props["name"])
+        self.cur.execute("MATCH (x:child) RETURN x")
+        x = self.cur.fetchone()[0]
+        self.assertEquals("son", x.props["name"])
+
+class MatchTest(BasicTest):
+    def setUp(self):
+        BasicTest.setUp(self)
+        self.cur.execute("create vlabel company")
+        self.cur.execute("create vlabel person")
+        self.cur.execute("create elabel employee")
+        self.cur.execute("create elabel manage")
+        self.cur.execute("create (:company {'name': 'bitnine'})"
+                + "-[:employee]"
+                + "->(:person {'name': 'kskim'})"
+                + "-[:manage]"
+                + "->(:person {'name': 'ktlee'})");
+        self.cur.execute("create (c:company {'name': 'bitnine'}) "
+                + "create (c)-[:employee]"
+                + "->(:person {'name': 'jsyang'})")
+        self.cur.execute("create (c:company {'name': 'bitnine'}) "
+                + ", (p:person {'name': 'ktlee'}) "
+                + "create (c)-[:employee]->(p)")
+        self.cur.execute("match (m:person {'name': 'kskim'})"
+                + ", (p:person {'name': 'jsyang'}) "
+                + "create (m)-[:manage]->(p)")
+    def test_Match(self):
+        self.cur.execute("MATCH (c)-[e]->(p1)-[m]->(p2) RETURN p1, p2 ORDER BY (p2).name")
+        row = self.cur.fetchone()
+        boss = row[0]
+        self.assertEquals("person", boss.label)
+        self.assertEquals("kskim", boss.props["name"])
+        member = row[1]
+        self.assertEquals("jsyang", member.props["name"])
+        row = self.cur.fetchone()
+        boss = row[0]
+        self.assertEquals("person", boss.label)
+        self.assertEquals("kskim", boss.props["name"])
+        member = row[1]
+        self.assertEquals("ktlee", member.props["name"])
+    def test_Path(self):
+        self.cur.execute("MATCH p=()-[]->()-[]->({'name':'ktlee'}) RETURN p")
+        p = self.cur.fetchone()[0]
+        self.assertEquals(["bitnine","kskim","ktlee"], 
+                [v.props["name"] for v in p.vertices])
+        self.assertEquals(["employee","manage"],
+                [e.label for e in p.edges])
+        self.assertEquals(2, p.len())
+
+class PropertyTest(BasicTest):
+    def setUp(self):
+        BasicTest.setUp(self)
+        self.cur.execute("create vlabel company")
+        self.cur.execute("create vlabel person")
+        self.cur.execute("create elabel employee")
+        self.cur.execute("create (:company {'name':'bitnine'})"
+                + "-[:employee {'no':1}]"
+                + "->(:person {'name':'jsyang', 'age':20, 'height':178.5, 'married':false})")
+        self.cur.execute("match (:company {'name':'bitnine'})"
+                + "create (c)-[:employee {'no':2}]"
+                + "->(:person '{\"name\":\"ktlee\", \"hobbies\":[\"reading\", \"climbing\"], \"age\":null}')")
+        self.cur.execute("create (:person {'name': 'Emil', 'from': 'Sweden', 'klout': 99})")
+    def test_Property(self):
+        self.cur.execute("match (n)-[:employee {'no':1}]->(m) RETURN n, m")
+        row = self.cur.fetchone()
+        self.assertEquals(20, row[1].props["age"])
+        self.assertEquals(178.5, row[1].props["height"])
+        self.assertFalse(row[1].props["married"])
+        self.cur.execute("match (n)-[:employee {'no':2}]->(m) RETURN n, m")
+        row = self.cur.fetchone()
+        self.assertEquals("climbing", row[1].props["hobbies"][1])
+        self.cur.execute("match (n)-[{'no':2}]->(m) return (m).hobbies as hobbies")
+        hobbies = self.cur.fetchone()[0]
+        self.assertEquals("reading", hobbies[0])
+        self.cur.execute("match (ee:person) where (ee).klout = to_jsonb(99::int) "
+                + "return (ee).name, (ee).name::text")
+        row = self.cur.fetchone()
+        self.assertIsInstance(row[0], unicode)
+        self.assertEquals("Emil", row[0])
+        self.assertIsInstance(row[1], str)
+        self.assertEquals('"Emil"', row[1])
+
+class ReturnTest(BasicTest):
+    def test_Return(self):
+        self.cur.execute("RETURN 'be' || ' happy!', 1+1")
+        row = self.cur.fetchone()
+        self.assertEquals("be happy!", row[0])
+        self.assertEquals(2, row[1])
+
+class WhereTest(BasicTest):
+    def setUp(self):
+        BasicTest.setUp(self)
+        self.cur.execute("create vlabel person")
+        self.cur.execute("create (:person { 'name': 'Emil', 'from': 'Sweden', 'klout': 99})")
+    def test_Where(self):
+        self.cur.execute("match (ee:person) where (ee).name = to_jsonb('Emil'::text) return ee")
+        row = self.cur.fetchone()[0]
+        self.assertEquals(99, row.props["klout"])
+        self.cur.execute("match (ee:person) where (ee).klout = to_jsonb(99::int) return ee")
+        row = self.cur.fetchone()[0]
+        self.assertEquals(99, row.props["klout"])
+    def test_WhereBind(self):
+        self.cur.execute("match (ee:person) where (ee).from = to_jsonb(%s::text) return ee", ('Sweden',))
+        row = self.cur.fetchone()[0]
+        self.assertEquals(99, row.props["klout"])
+        self.cur.execute("match (ee:person {'klout': %s}) return (ee).name", (99,))
+        row = self.cur.fetchone()[0]
+        self.assertIsInstance(row, unicode)
+        self.assertEquals("Emil", row)
+    def test_WhereBindJson(self):
+        self.cur.execute("match (ee:person) where (ee).name = %s return ee",
+                (Json("Emil"),))
+        row = self.cur.fetchone()[0]
+        self.assertIsInstance(row, agtype.Vertex)
+        self.assertEquals("Emil", row.props["name"])
+    def test_MatchBindStr(self):
+        self.cur.execute("match (ee:person %s) return ee", 
+                ("{\"name\": \"Emil\"}",))
+        row = self.cur.fetchone()[0]
+        self.assertEquals(99, row.props["klout"])
+    def test_MatchBindJson(self):
+        self.cur.execute("match (ee:person %s) return ee", 
+                (Json({'name': 'Emil'}),))
+        row = self.cur.fetchone()[0]
+        self.assertEquals(99, row.props["klout"])
+
+if __name__ == '__main__':
+    unittest.main()
